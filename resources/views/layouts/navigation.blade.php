3abe3be97fc4697dbc24d9e0661d582665a74cb9
Steffen Kaster <skaster@kaster-development.de>
2021-01-04 05:42:52 +0100

    Create index.storage.blade.php an add to navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('index-storage')" :active="request()->routeIs('index-storage')">
+                        {{ __('Storage') }}
+                    </x-nav-link>
                 </div>
             </div>
 
