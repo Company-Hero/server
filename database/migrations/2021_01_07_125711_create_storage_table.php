23c54d06341f654779511263b9d033ca1639a16c
Steffen Kaster <skaster@kaster-development.de>
2021-01-07 14:02:18 +0100

    Create Storage datatbase

---
+++ database/migrations/2021_01_07_125711_create_storage_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStorageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storage', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('street')->nullable();
+            $table->string('postcode')->nullable();
+            $table->string('place')->nullable();
+            $table->string('country')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storage');
+    }
+}
